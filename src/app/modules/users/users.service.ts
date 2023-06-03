775e379486b7ccc53c5c26d41e875442e976aaa7
Delwar Ahmed <delwar.habib@gmail.com>
2023-06-03 22:52:36 +0600

    [DONE] Created User Service

---
+++ src/app/modules/users/users.service.ts
@@ -0,0 +1,27 @@
+import config from '../../../config'
+import { generateUserId } from './user.utils'
+import { IUser } from './users.interface'
+import { User } from './users.model'
+
+const createUser = async (user: IUser): Promise<IUser | null> => {
+  // auto generated user id
+  const id = await generateUserId()
+
+  user.id = id
+
+  // default password
+  if (!user.password) {
+    user.password = config.default_user_password as string
+  }
+
+  const createdUser = await User.create(user)
+
+  if (!createUser) {
+    throw new Error(`Failed to create user!`)
+  }
+  return createdUser
+}
+
+export default {
+  createUser,
+}
